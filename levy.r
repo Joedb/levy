12f146d80f71b6993edec52de480e649bd73976f
Guiseppe DiBenedetto <benedett@thornbird.stats.ox.ac.uk>
2016-03-03 17:58:06 +0000

    plots worth printing

---
+++ levy.r
@@ -0,0 +1,54 @@
+require('sde')
+require('actuar')
+
+stable_process <- function(t_in, t_fin, ngrid, epsilon, sigma) {
+# Simulation of jumps
+  M <- epsilon ^ (- sigma) / sigma
+  N <- rpois(1, t_fin * M)
+  time_jump<- runif(N, t_in, t_fin)
+  delta_x <- rpareto(N, sigma, epsilon)
+
+  time <- seq(t_in, t_fin, length.out = ngrid)
+  time <- c(time_jump, time)
+  index <- order(time)
+  time <- sort(time)
+  X <- c(0, rep(- (t_fin - t_in) / ngrid, ngrid - 1))
+  mu_epsilon <- (1 - epsilon ^ (1 - sigma)) / (1-sigma)
+  X <- c(delta_x, X)
+  X <- X[index]
+  X <- cumsum(X)
+  plot(time, X)
+  return (N)
+}
+
+stable_process(0,5,1000,0.01,0.5)
+
+
+
+gamma_process <- function(t_in, t_fin, ngrid, epsilon, tau, sigma) {
+# Simulation of jumps
+  M <- tau ^ (-sigma) * gamma(sigma) * (1 - dgamma(tau * epsilon, sigma))
+  N <- rpois(1, t_fin * M)
+  time <- runif(N, t_in, t_fin)
+  delta_x < <- rep(0,N)
+  for (i in 1:N) {
+    x <- rgamma(1, shape = sigma, rate = tau)
+    while (x < epsilon)
+      x <- rgamma(1, shape = sigma, rate = tau)
+    delta[i] <- x
+  }
+
+  time <- seq(t_in, t_fin, length.out = ngrid)
+  time <- c(time_jump, time)
+  index <- order(time)
+  time <- sort(time)
+  X <- c(0, rep(- (t_fin - t_in) / ngrid, ngrid - 1))
+  mu_epsilon <- (1 - epsilon ^ (1 - sigma)) / (1-sigma)
+  X <- c(delta_x, X)
+  X <- X[index]
+  X <- cumsum(X)
+  plot(time, X)
+  return (N)
+}
+
+gamma_process(0,5,1000,0.01,0.5)
