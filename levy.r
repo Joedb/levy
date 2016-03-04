f484d92eaf89f49a69081d4d15c9464fa530dc9e
Guiseppe DiBenedetto <benedett@thornbird.stats.ox.ac.uk>
2016-03-04 14:44:49 +0000

    kinda gamma

--- levy.r
+++ levy.r
@@ -1,3 +1,4 @@
+
 require('sde')
 require('actuar')
 
@@ -28,15 +29,18 @@ stable_process(0,5,1000,0.01,0.5)
 
 gamma_process <- function(t_in, t_fin, ngrid, epsilon, tau, sigma) {
 # Simulation of jumps
-  M <- tau ^ (-sigma) * gamma(sigma) * (1 - dgamma(tau * epsilon, sigma))
-  N <- rpois(1, t_fin * M)
-  time <- runif(N, t_in, t_fin)
-  delta_x < <- rep(0,N)
+  density <- function(x)
+    return (x^(-1-sigma) * exp(-tau * x))  
+  M <- integrate(density, epsilon, Inf)[[1]]
+  
+  N <- rpois(1, (t_fin - t_in) * M)
+  time_jump <- runif(N, t_in, t_fin)
+  delta_x <- rep(0,N)
   for (i in 1:N) {
-    x <- rgamma(1, shape = sigma, rate = tau)
-    while (x < epsilon)
-      x <- rgamma(1, shape = sigma, rate = tau)
-    delta[i] <- x
+    x <- rpareto(1, sigma, epsilon)
+    while (runif(1, 0, (epsilon^(-sigma) / sigma) * dpareto(x, sigma, epsilon) > density(x)))
+      x <- rpareto(1, sigma, epsilon)
+    delta_x[i] <- x / M
   }
 
   time <- seq(t_in, t_fin, length.out = ngrid)
@@ -44,7 +48,12 @@ gamma_process <- function(t_in, t_fin, ngrid, epsilon, tau, sigma) {
   index <- order(time)
   time <- sort(time)
   X <- c(0, rep(- (t_fin - t_in) / ngrid, ngrid - 1))
-  mu_epsilon <- (1 - epsilon ^ (1 - sigma)) / (1-sigma)
+  mean_jump_dens <- function(x)
+     return (x^(-sigma) * exp(-tau * x))  
+  mu_epsilon <- integrate(mean_jump_dens, epsilon, 1)[[1]]
+
+  X <- c(0, rep( mu_epsilon * (t_fin - t_in) / ngrid, ngrid - 1))
+#  X <- rep(0, ngrid)
   X <- c(delta_x, X)
   X <- X[index]
   X <- cumsum(X)
@@ -52,4 +61,16 @@ gamma_process <- function(t_in, t_fin, ngrid, epsilon, tau, sigma) {
   return (N)
 }
 
-gamma_process(0,5,1000,0.01,0.5)
+gamma_process(0,1,1000,0.01,0.5,0.5)
+
+
+density <- function(x)
+    return (x^(-1-0.5) * exp(-0.5 * x))  
+(M <- integrate(density, 0.01, Inf))
+class(M)
+as.double(M)
+M[[1]]
+
+mean_jump_dens <- function(x)
+  return (x^(-0.5) * exp(-0.5 * x)) 
+(mu_epsilon <- integrate(mean_jump_dens, 0.01, 1)[[1]])
